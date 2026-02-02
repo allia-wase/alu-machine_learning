717e029bd067294e9b66379e0947bde3f9e90446
allia-wase <a.umutoniwa1@alustudent.com>
2026-02-02 18:31:16 +0200

    Revert: restore NumPy example and prints for grader

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Matrix slicing helper functions (no numpy)"""
-def get_middle_rows(m):  # noqa
-    return [] if not m else [row[:] for row in m[1:3]]
-
-def get_middle_columns(m):  # noqa
-    return [] if not m else [[r[2], r[3]] for r in m]
-
-def get_bottom_right(m, size=3):  # noqa
-    return [] if not m else [r[-size:] for r in m[-size:]]
+import numpy as np
+matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
+                   [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
+mat1 = matrix[1:3]
+mat2 = matrix[:, 2:4]
+mat3 = matrix[-3:, -3:]
+print("The middle two rows of the matrix are:\n{}".format(mat1))
+print("The middle two columns of the matrix are:\n{}".format(mat2))
+print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
