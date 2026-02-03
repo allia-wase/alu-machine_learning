a35d3afa69d1d5909b3233a8bf0c5cf982bafc0f
allia-wase <a.umutoniwa1@alustudent.com>
2026-02-03 13:34:56 +0200

    Add math/pipeline/supervised/unsupervised tasks and update linear_algebra

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """implements the np_matmul function."""
 
+import numpy as np
+
 
 def np_matmul(mat1, mat2):
-    """ Performs matrix multiplication on two np.ndarrays.
-    Every row is multiplied by every column.
-    """
-    return mat1 @ mat2
+    """Performs matrix multiplication on two numpy arrays."""
+    return np.matmul(mat1, mat2)
